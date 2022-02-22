cc8ee7acc393eea586827b2e748b0100a301223b
John Palenchar <jpalenchar.3@gmail.com>
2022-02-22 15:33:02 -0500

    init commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path');
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    fullscreen: true,
+    webPreferences: {
+      devTools: false
+    }
+  });
+
+	mainWindow.setMenu(null);
+	mainWindow.loadFile(path.join(__dirname, 'index.html'));
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+})
+;
+
